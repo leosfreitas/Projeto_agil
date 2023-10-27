c4a668d2db774cfffe6d2f99a3ab32a4bfba2b75
leosfreitas <leonardosf@al.insper.edu.br>
2023-10-27 11:06:22 -0300

    cardapio tem back

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo, ObjectId
+import pymongo
+
+app = Flask(__name__)
+mongo_url = "mongodb+srv://admin:admin@projetoagil.3bq3al9.mongodb.net/"
+client = pymongo.MongoClient(mongo_url)
+db = client["App"]
+collection = db['cardapio']
+
+pedidos_collection = db['pedidos']
+
+@app.route('/cardapio', methods=['GET'])
+def get_cardapio():
+    try:
+        filter_ = {}
+        projection_ = {}
+        cardapio = list(collection.find(filter_, projection_))
+        for prato in cardapio:
+            prato["_id"] = str(prato["_id"])
+        return {"cardapio": cardapio}, 200
+    except Exception as e:
+        return {"erro":str(e)}, 500
+        
+@app.route('/pedidos', methods=['POST'])
+def inserir_pedido():
+    try:
+        data = request.json
+        id_pedido = pedidos_collection.insert_one(data)
+        print(id_pedido.inserted_id)
+        return {"_id": str(id_pedido.inserted_id)}, 201
+    except Exception as e:
+        return {"erro":str(e)}, 500
+    
+if __name__ == "__main__":
+    app.run(debug=True)
--- pages/cardapio.py
+++ pages/cardapio.py
@@ -1,18 +1,11 @@
 import streamlit as st
 import pymongo
-
-mongo_url = "mongodb+srv://admin:admin@projetoagil.3bq3al9.mongodb.net/"
-client = pymongo.MongoClient(mongo_url)
-db = client["App"]
-collection = db['cardapio']
-
-selected_collection = db['pedidos']
-
-total_pedidos = selected_collection.count_documents({})
+import requests
 
 pratos_selecionados = {}
 st.title("Cardápio")
-pratos_info = list(collection.find())
+
+pratos_info = requests.get('http://localhost:5000/cardapio').json()['cardapio']
 for prato_info in pratos_info:
     nome_prato = prato_info["name"]
     quantidade_sel = st.number_input(f"{nome_prato}", min_value=0, max_value=4, value=0)
@@ -33,13 +26,6 @@ if st.button('Adcionar pedido'):
         quantidade = dados['quantidade']
         nomes.append(nome)
         quantidades.append(quantidade)
-    data = {
-        "id": total_pedidos + 1,
-        "nome": nomes,
-        "quantidade": quantidades,
-        "status": "Em preparo"
-    }
-    pedido_id = selected_collection.insert_one(data)
+    inserir_prato = requests.post('http://localhost:5000/pedidos', json={"id": 0, "nome": nomes, "quantidade": quantidades, "status": "Em preparo"})
     st.write("Pedido adicionado com sucesso")
-
-client.close()
+    pratos_selecionados.clear()
